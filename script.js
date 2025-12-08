72cdd52e8e2f592068db94d900e515249b094ef8
aiau <zhumakanaiauly@gmail.com>
2025-12-08 08:56:16 +0500

    Add 'gender_party' label to labels object

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ ready(() => {
       woman: "Девушке",
       birth: "День Рождения",
       popular: "Популярные товары",
+      gender_party: "Гендер party",
     };
     const t = labels[key] || labels.all;
     if (headerTitleEl) {
